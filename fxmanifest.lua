62e88291806ba525e92172ca9bade9d350ced27e
kibook <kibuzhj@gmail.com>
2021-09-07 10:40:20 -0400

    Add metadata to manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,5 +2,10 @@ fx_version "cerulean"
 game "rdr3"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 
+name "flintlock"
+author "kibukj"
+description "Equip a flintlock pistol"
+repository "https://github.com/kibook/redm-flintlock"
+
 server_script "server.lua"
 client_script "client.lua"
